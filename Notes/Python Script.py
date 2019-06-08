9464e624e7af9e531616593c51d851bc84ea9aaf
Pranav Jain <pranav19sept@gmail.com>
2019-06-08 09:23:39 +0530

    08-06-19

--- Notes/Python Script.py
+++ Notes/Python Script.py
@@ -18,3 +18,48 @@ t = a + b + c
 print('{0:10d}\n{1:10d}\n{2:10d}\n{3:10d}'.format(a, b, c, t))
 #Allignment
 print('{0:<10d}\n{1:>10d}\n{2:^10d}\n{3:10d}'.format(a, b, c, t))
+
+#input
+a = input('Enter number: ')
+b = input('ENter number: ')
+c = a + b
+#c = a - b //not possible
+#c = a * b //not possible
+a = int(input('Enter number: ')) #same for float
+b = int(input('Enter number: '))
+c = a + b
+#if
+amt = float(input('Enter Amount: '))
+qty = float(input('Enter Quantity'))
+tamt = amt * qty
+print('Amount: ', tamt)
+if(tamt >= 100000):
+    dis = tamt * 20/100
+elif(tamt >= 20000 and tamt <= 99999):
+    dis = tamt * 10/100
+else:
+    dis = tamt * 5/100
+print('Discount Amount: ', dis)
+netamt = tamt - dis
+print('Net Amount: ', netamt)
+#while
+n = int(input('Enter number'))
+i = 1
+while i <= 10:
+    t = n * i
+    print(n, 'x', i, ' = ', t)
+    i += 1
+
+#nested while
+i = 1
+while(i <= 5):
+    j = 1
+    while(j <= 5):
+        print(j, end='')
+        j = j+1
+    i = i+1
+    print()
+
+#for
+for i in range(1, 11):
+    print(i)
